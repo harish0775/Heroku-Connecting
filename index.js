9a8a7df2dd0e93bf22f5add362c9ef67ff5fc129
Harish Nishad <harish.nishad0775@gmail.com>
2021-01-03 23:26:35 +0530

    env.is added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 11000;
+const port = process.env.Port || 11000;
 const expressLayouts = require('express-ejs-layouts'); 
 const db = require('./config/mongoose');
 // used for session cookie
